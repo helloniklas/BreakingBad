857fde4189e457d90e5d2e6f59b9f671b2b41635
Niklas Alvaeus <niklas@helloniklas.com>
2022-04-22 11:19:07 +0100

    Initial Commit

---
+++ Shared/BreakingBadApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadApp.swift
+//  Shared
+//
+//  Created by Niklas Alvaeus on 22/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
